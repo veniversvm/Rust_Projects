0e073662452f65a7ac3240dae0d354d1265dc2d9
FranSabt <franhsabt@gmail.com>
2026-04-25 21:28:50 -0400

    multithreding server

--- hello/target/debug/hello.d
+++ hello/target/debug/hello.d
@@ -1,1 +1,1 @@
-/home/fran/Rust_Projects/hello/target/debug/hello: /home/fran/Rust_Projects/hello/src/main.rs
+/home/fran/Rust_Projects/hello/target/debug/hello: /home/fran/Rust_Projects/hello/src/lib.rs /home/fran/Rust_Projects/hello/src/main.rs
