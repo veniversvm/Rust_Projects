357e691e688d433946ac846e1401a39a5558a071
FranSabt <franhsabt@gmail.com>
2026-05-15 21:55:37 -0400

    die test passed

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,4 +1,5 @@
 use clap::{App, Arg};
+use std::{default, fs};
 use std::{error::Error, ffi::c_long};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -16,43 +17,61 @@ pub fn get_args() -> MyResult<Config> {
         .author("yo")
         .about("Rust Head")
         .arg(
-            Arg::with_name("files")
-                .value_name("FILE")
-                .help("Input text")
-                .required(true)
-                .min_values(1),
-        )
-        .arg(
             Arg::with_name("lines")
                 .short("n")
-                .long("--lines")
+                .long("lines")
+                .value_name("LINES")
                 .help("Select how many lines")
                 .required(false)
-                .takes_value(true),
-            //.default_value(10 as usize),
+                .takes_value(true), //.conflicts_with("bytes"),
+                                    //.default_value(10 as usize),
         )
         .arg(
             Arg::with_name("bytes")
                 .short("c")
-                .long("--bytes")
+                .long("bytes")
+                .value_name("BYTES")
                 .help("Select how many bytes")
                 .required(false)
-                .takes_value(true),
+                .takes_value(true)
+                .conflicts_with("lines"),
+        )
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input text")
+                .required(true)
+                .min_values(1)
+                .multiple(true)
+                .default_value("-"),
         )
         .get_matches();
 
     let lines = match matches.value_of("lines") {
-        Some(v) => parse_positive_int(v)?,
+        Some(v) => parse_positive_int(v).map_err(|e| format!("illegal line count -- {}", e))?,
         None => 10, //Err(e),
+    };
+
+    let bytes_value = match matches.value_of("bytes") {
+        Some(v) => parse_positive_int(v).map_err(|e| format!("illegal byte count -- {}", e))?,
+        None => 0,
+    };
+
+    let bytes = if bytes_value > 0 {
+        Some(bytes_value)
+    } else {
+        None
     };
 
     let file = matches.values_of_lossy("files").unwrap();
     println!("{:?}", file);
 
+    get_folder_content(file[0].clone());
+
     Ok(Config {
         files: vec!["-".to_string()],
         lines,
-        bytes: None,
+        bytes,
     })
 }
 
@@ -75,6 +94,15 @@ fn parse_positive_int(val: &str) -> MyResult<usize> {
     }
 }
 
+fn get_folder_content(file: String) {
+    //file.insert_str(0, "./");
+    println!("searchin in folder: {}", file);
+    let paths = fs::read_dir(file).unwrap();
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display());
+    }
+}
+
 #[test]
 fn test_parse_positive_int() {
     let res = parse_positive_int("3");
@@ -89,3 +117,6 @@ fn test_parse_positive_int() {
     assert!(res.is_err());
     assert_eq!(res.unwrap_err().to_string(), "0".to_string());
 }
+
+#[test]
+fn test_content_files() {}
